2ed83d9f4838665f63c9dfc65f7ab38abfc9e818
Kolawole <onaelmangabo@gmail.com>
2021-08-04 21:01:41 +0100

    - feat: add the user model

--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, PermissionsMixin, AbstractBaseUser)
+from core.abstract.models import AbstractModel, AbstractManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager, AbstractManager):
+
+    def create_user(self, username, password):
+
+        if username is None:
+            raise TypeError("You should provide an username.")
+
+        if password is None:
+            raise TypeError("You should put a password")
+
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password):
+
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractModel, PermissionsMixin, AbstractBaseUser):
+    username = models.CharField(max_length=255, null=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
