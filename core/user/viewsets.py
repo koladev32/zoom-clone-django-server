212a28612b587833911ceb1af8d7344df34a60fe
Kolawole <onaelmangabo@gmail.com>
2021-08-04 21:05:43 +0100

    - feat: add the user viewset

---
+++ core/user/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from core.user.serializers import UserSerializer
+
+
+# TODO: check if this requires custom permission
+
+class UserViewSet(ModelViewSet):
+    http_method_names = ('post', 'get')
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
