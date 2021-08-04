ec3aa983392390f654565eb45789bfa911445a9f
Kolawole <onaelmangabo@gmail.com>
2021-08-04 20:51:10 +0100

    - wip: add todo for room

--- core/room/models.py
+++ core/room/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from core.abstract.models import AbstractModel, AbstractManager
 
 
-class Room(models.Model):
+class RoomManager(AbstractManager):
+    pass
+
+
+class Room(AbstractModel):
     name = models.CharField(max_length=255)
+
+    # TODO : Add moderator which will be an authenticated user user
