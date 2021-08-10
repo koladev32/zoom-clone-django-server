c34fe49f4b8801254f6292c46ce105beaea221eb
koladev <onaelmangabo@gmail.com>
2021-08-10 22:25:13 +0100

    - chore: add .dockerignore

--- ZoomClone/asgi.py
+++ ZoomClone/asgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-import os
+from .wsgi import *  # add this line to top of your code
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
