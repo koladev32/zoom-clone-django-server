9315b0f9c611003775054949f0f76f6190f96060
koladev <onaelmangabo@gmail.com>
2021-08-20 04:46:14 +0100

    - feat: add room serializers

--- core/room/serializers.py
+++ core/room/serializers.py
@@ -1,11 +1,31 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from core.room.models import Room
+from core.user.models import User
 
 
-# TODO: Add the moderator id
 
 class RoomSerializer(serializers.ModelSerializer):
+    creator = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='public_id'
+    )
+
+    def validate(self, attrs):
+        creator = attrs.get('creator')
+
+        if not creator.is_active:
+            raise ValidationError({'user': 'This user can\'t host a room.'})
+
+        try:
+            Room.objects.get(creator=creator, status='active')
+            raise ValidationError({'user': 'This user is hosting a room.'})
+        except ObjectDoesNotExist:
+            return attrs
+
     class Meta:
         model = Room
-        fields = ['public_id, name, created, updated']
+        fields = ['public_id', 'creator', 'name', 'created', 'updated']
