ecfef93f2371ba017f2098ba1c2a6ad24ddc03f6
koladev <onaelmangabo@gmail.com>
2021-08-21 07:29:23 +0100

    - chore: add database settings

--- ZoomClone/settings.py
+++ ZoomClone/settings.py
@@ -86,12 +86,32 @@ ASGI_APPLICATION = 'ZoomClone.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+DATABASE_CONFIG_GITHUB_WOKRLFOW = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
     }
-}
+
+
+DATABASE_CONFIG_DATABASE = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("DATABASE_NAME", 'zoom'),
+            'USER': os.environ.get("DATABASE_USER", 'zoom'),
+            'PASSWORD': os.environ.get("DATABASE_PASSWORD", 'ZOOM12345'),
+            'HOST': os.environ.get("DATABASE_HOST", '127.0.0.1'),
+            'PORT': os.environ.get("DATABASE_PORT", '5432'),
+        }
+    }
+
+DATABASES = DATABASE_CONFIG_GITHUB_WOKRLFOW if os.environ.get('GITHUB_WORKFLOW') else DATABASE_CONFIG_DATABASE
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
