73ce49bad0869e8e1e0d89ae637ade45197c7596
koladev <onaelmangabo@gmail.com>
2021-08-21 07:01:03 +0100

    - fix: hashing password

--- core/fixtures/user.py
+++ core/fixtures/user.py
@@ -1,9 +1,11 @@
 from core.user.models import User
+from django.contrib.auth.hashers import make_password
 
 data_user = {
     "username": "",
     "is_active": True,
-    "is_superuser": False
+    "is_superuser": False,
+    "password": make_password("12345")
 }
 
 User.objects.create_user(**data_user)
