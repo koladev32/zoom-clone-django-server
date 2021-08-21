1fd24b436a7419ba35efc54b1769257b0209a1a2
koladev <onaelmangabo@gmail.com>
2021-08-21 16:18:05 +0100

    - test: add test for room

--- core/room/serializers.py
+++ core/room/serializers.py
@@ -7,12 +7,12 @@ from core.room.models import Room
 from core.user.models import User
 
 
-
 class RoomSerializer(serializers.ModelSerializer):
     creator = serializers.SlugRelatedField(
         queryset=User.objects.all(),
         slug_field='public_id'
     )
+    name = serializers.CharField(max_length=35, min_length=5, required=True)
 
     def validate(self, attrs):
         creator = attrs.get('creator')
@@ -29,3 +29,4 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ['public_id', 'creator', 'name', 'created', 'updated']
+        read_only_fields = ['public_id']
--- core/room/tests.py
+++ core/room/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from core.user.models import User
+
+
+class UserTest(APITestCase):
+    base_url_room = reverse("core-api:room-list")
+    base_url_login = reverse("core-api:auth-login-list")
+
+    data_login = {"password": "12345678", "username": "koladev32"}
+
+    data_room = {"name": "room-test"}
+
+    def test_create_room(self):
+        response = self.client.post(f"{self.base_url_login}", data=self.data_login)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = response.json()
+
+        access = response_data["access"]
+        user_public_id = response_data["user"]["public_id"]
+
+        self.data_room['creator'] = user_public_id
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + access)
+
+        response = self.client.post(f"{self.base_url_room}", data=self.data_room)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
