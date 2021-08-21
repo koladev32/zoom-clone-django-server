4ca0614ad26d8bd928933fc5fbeee88708d8223d
koladev <onaelmangabo@gmail.com>
2021-08-21 15:47:38 +0100

    - feat: add viewsets get_queryset and get_object

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -1,11 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from django.http import Http404
 
 from core.authentication.permissions import UserPermission
 from core.user.serializers import UserSerializer
+from core.user.models import User
 
 
 class UserViewSet(ModelViewSet):
     http_method_names = ['get']
-    permission_classes = (IsAuthenticated, UserPermission)
+    permission_classes = (UserPermission,)
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all().exclude(is_superuser=True)
+        return Http404
+
+    def get_object(self):
+        lookup_field_value = self.kwargs[self.lookup_field]
+        if lookup_field_value == 'current':
+            obj = self.request.user
+        else:
+            obj = User.objects.get_by_public_id(lookup_field_value)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
