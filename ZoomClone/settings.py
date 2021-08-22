905cb7dace31c4908c9b62e3d99627361d1d2a86
koladev <onaelmangabo@gmail.com>
2021-08-22 18:07:14 +0100

    Revert "- fix: temporary fix"
    This reverts commit e71577e8ac892be019e62d9e8ceb5bc25e65f519.

--- ZoomClone/settings.py
+++ ZoomClone/settings.py
@@ -185,17 +185,11 @@ TEST_RUNNER = "core.xlibs.testing.CoreTestRunner"
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'redis://localhost:6379')
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": os.environ.get('CHANNEL_LAYERS_BACKEND', "channels_redis.core.RedisChannelLayer"),
-#         "CONFIG": {
-#             "hosts": [REDIS_HOST],
-#         },
-#     }
-# }
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "BACKEND": os.environ.get('CHANNEL_LAYERS_BACKEND', "channels_redis.core.RedisChannelLayer"),
+        "CONFIG": {
+            "hosts": [REDIS_HOST],
+        },
     }
 }
