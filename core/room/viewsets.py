40ad49d42bdc77617e0356baee001df2ab13501b
koladev <onaelmangabo@gmail.com>
2021-08-22 18:08:15 +0100

    - fix: add queryset

--- core/room/viewsets.py
+++ core/room/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from core.room.serializers import RoomSerializer
+from core.room.models import Room
 from rest_framework.permissions import AllowAny
 
 
@@ -8,3 +9,6 @@ class RoomViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post']
     serializer_class = RoomSerializer
     permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        return Room.objects.filter(status='active')
